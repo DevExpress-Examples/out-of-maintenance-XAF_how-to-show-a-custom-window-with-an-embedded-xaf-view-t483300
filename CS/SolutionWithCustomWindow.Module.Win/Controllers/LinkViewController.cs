9bc688c4850315632f211bb7dbc9ea3edd55f122
kashiash <kashiash@gmail.com>
2018-11-22 12:40:00 +0100

    DetailView in Non-Xaf Form
    Add example how to use DetailView

--- CS/SolutionWithCustomWindow.Module.Win/Controllers/LinkViewController.cs
+++ CS/SolutionWithCustomWindow.Module.Win/Controllers/LinkViewController.cs
@@ -2,16 +2,64 @@
 using DevExpress.ExpressApp.Actions;
 using DevExpress.Persistent.BaseImpl;
 using DevExpress.XtraLayout;
+using System;
 using System.Windows.Forms;
 
 namespace SolutionWithCustomWindow.Module.Win.Controllers {
     public partial class LinkViewController : ViewController {
         public LinkViewController() {
-            SimpleAction showWindowAction = new SimpleAction(this, "Show Window", DevExpress.Persistent.Base.PredefinedCategory.View);
-            showWindowAction.ImageName = "ModelEditor_Views";
-            showWindowAction.Execute += new SimpleActionExecuteEventHandler(showWindowAction_Execute);
+            SimpleAction showLVWindowAction = new SimpleAction(this, "Show ListView", DevExpress.Persistent.Base.PredefinedCategory.View);
+            showLVWindowAction.ImageName = "ModelEditor_Views";
+            showLVWindowAction.Execute += new SimpleActionExecuteEventHandler(showLVWindowAction_Execute);
+
+
+            SimpleAction showDVWindowAction = new SimpleAction(this, "Show DetailView", DevExpress.Persistent.Base.PredefinedCategory.View);
+            showDVWindowAction.ImageName = "ModelEditor_Views";
+            showDVWindowAction.Execute += new SimpleActionExecuteEventHandler(showDVWindowAction_Execute);
+
         }
-        void showWindowAction_Execute(object sender, SimpleActionExecuteEventArgs e) {
+
+        private void showDVWindowAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            NonXAFForm form = new NonXAFForm();
+            form.Text = "Form with the XAF DetailView";
+
+            LayoutControl layoutControl = new LayoutControl();
+            layoutControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            form.Controls.Add(layoutControl);
+
+           // LayoutControlItem item1 = layoutControl.Root.AddItem();
+
+
+
+            IObjectSpace os = Application.CreateObjectSpace();
+            Person record = os.GetObject((Person)View.CurrentObject);
+
+            if (record == null)
+            {
+                record = os.CreateObject<Person>();
+            }
+            DevExpress.ExpressApp.DetailView view = Application.CreateDetailView(os, "Person_DetailView", true, record);
+                view.ViewEditMode = DevExpress.ExpressApp.Editors.ViewEditMode.Edit;
+            //view.CreateControls();
+            //LayoutControlItem item2 = layoutControl.Root.AddItem();
+            //item2.Text = "Persons";
+            //item2.Control = (Control)view.Control;
+
+            //form.ShowDialog();
+
+            Frame frame = Application.CreateFrame(TemplateContext.NestedFrame);
+            frame.CreateTemplate();
+            frame.SetView(view);
+            LayoutControlItem item2 = new LayoutControlItem();
+            item2.Parent = layoutControl.Root;
+            item2.Text = "Persons";
+            item2.Control = (Control)frame.Template;
+            form.ShowDialog();
+
+        }
+
+        void showLVWindowAction_Execute(object sender, SimpleActionExecuteEventArgs e) {
             NonXAFForm form = new NonXAFForm();
             form.Text = "Form with the XAF ListView";
   
